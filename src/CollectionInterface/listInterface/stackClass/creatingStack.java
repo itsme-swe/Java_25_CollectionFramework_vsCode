1be1275ca1137a8dfb89313aeabb3bb8cd01656f
itsme-swe <me.harshmehra07@gmail.com>
2024-11-11 07:46:36 +0530

    update

---
+++ src/CollectionInterface/listInterface/stackClass/creatingStack.java
@@ -0,0 +1,14 @@
+package CollectionInterface.listInterface.stackClass;
+
+import java.util.Stack;
+
+public class creatingStack {
+  public static void main(String[] args) {
+
+    Stack<Integer> st = new Stack<>();
+
+    st.push(10); // add an element in stack
+
+    System.out.println(st); // Output: [10]
+  }
+}
